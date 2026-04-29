62005d7c3495469ae592f00bf035310897c939b6
fredjrp <110814718+fredjrp@users.noreply.github.com>
2026-04-29 16:11:59 +0300

    Update firebase_options.dart

--- uber_drivers_app/lib/firebase_options.dart
+++ uber_drivers_app/lib/firebase_options.dart
@@ -7,10 +7,10 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions windows = FirebaseOptions(
-    apiKey: 'DUMMY-API-KEY-FOR-BUILDING',
+    apiKey: 'AIzaSyAk9DOEc_hmyeAJEob8QBAyQkiZCp_CPuI',
     appId: '1:805708188196:android:016756c8de3f655b19a6b1',
-    messagingSenderId: '1234567890',
+    messagingSenderId: '805708188196',
     projectId: 'uberzo',
-    storageBucket: 'uberzo.appspot.com',
+    storageBucket: 'uberzo.firebasestorage.app',
   );
 }
